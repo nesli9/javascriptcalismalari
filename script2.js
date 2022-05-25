0304a4a3d2444d05f13a15eb054750e9b16863f1
nesli9 <doanneslihan@gmail.com>
2022-05-25 21:06:33 +0300

    script3

--- script2.js
+++ script2.js
@@ -1,11 +1,107 @@
-var age;
-console.log(age);
-age=20;
-console.log(age);
+// var age;
+// console.log(age);
+// age=20;
+// console.log(age);
 
-var fullname='sadık turan';
-console.log(fullname);
+// var fullname='sadık turan';
+// console.log(fullname);
 
-fullname='çınar turan';
-console.log(fullname);
+// fullname='çınar turan';
+// console.log(fullname);
+
+
+// Değişken Tipleri
+//primitive types
+//String
+//let firstName='Çınar';
+
+//number
+// let age=20;
+// let money=100.5;
+
+//boolean
+//let isActive = true ;
+
+//null
+//let job = null;
+
+//underfined
+//let car ;
+
+
+
+
+
+//referances types
+//array
+//let names = ['ali','ahmet','can'];
+
+//object
+// let adress={
+//     city:'kocaeli',
+//     country:'türkiye'
+// };
+
+//function
+// var calculate = function(){
+//     return 0;
+// };
+
+
+//console.log(typeof calculate);
+
+//TÜR DÖNÜŞÜMÜ
+
+// let num1 = '5';
+// let num2 = '10';
+
+// let num1= Number('5');
+// let num2=Number('10');
+
+// let total = num1+num2;
+
+let val;
+
+//number to string
+val= String(10);
+
+//bolean to string
+val =String(true);
+
+//date to string
+val= String(new Date()); // date le tarih zaman biligilerini görebiliriz.
+
+//array to string
+val = String([1,2,3,4]);
+
+//toString()
+val = (10).toString();
+
+//String to Number
+val = Number('10');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('a');
+val = Number([1,2,3,4]);
+
+//parseInt
+//parseFloat
+
+val = parseInt('10');
+val = parseInt('10.5'); //ondalık bir sayı tam sayıya yuvarlanmış şekilde gösterilir.
+val = parseFloat('10.5');
+
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length); //karakter uzunluğu
+
+
+
+
+
+
+// console.log(num1+num2); // num1 ve num2 deki değerler string olarak birleştirildi (yan yana yazıldı)
+// console.log(typeof num1);
 
---
+++ script3.js
@@ -0,0 +1,61 @@
+//** Aşağıdaki Müşteri bilgileri için değişken oluşturma
+
+// **Müşteri adı
+// **Müşteri soyadı
+// **Müşteri tc no
+// **Müşteri sipariş toplam
+// **Müşteri cinsiyet
+// **Müşteri adres bilgisi
+// **Müşteri hobiler
+
+var customerName = 'çınar';
+var customerLastName='turan';
+
+var fullName='çınar turan';
+var musteriId = '12365478912';
+var total= 205.6;
+var gender = 'erkek'; // var gender = true;  (kadın true , erkek false)
+//object türü
+var adres = {
+    city :'Kocaeli',
+    district :'izmit',
+    body:'abc mah.'
+}
+//array türü
+var hobbies = ['sinema','kitap','spor']
+
+
+
+
+//** Sipariş toplamı
+
+var order1= Number('100');
+var order2= Number('50');
+
+var totalOrder = order1 + order2;
+console.log(totalOrder);
+
+var order3 = Number('100.2');
+var order4 = Number('150.5'); // ondalıklı olarak toplanır
+
+var totalOrder2 = order3 + order4;
+console.log(totalOrder2);
+
+var order5 = parseInt('100.2');
+var order6 = parseInt('150.5'); //tam sayı olarak toplamak için
+
+var totalOrder3 = order5 + order6;
+console.log(totalOrder3);
+
+//doğum yılına göre yaş hesaplama
+
+var yearOfBirth = 1986;
+
+console.log(new Date().getFullYear()-yearOfBirth);
+
+// ifadenin karakter sayısını bulma
+
+var course = 'Modern Javascript Kursu';
+
+console.log(course.length);
+
