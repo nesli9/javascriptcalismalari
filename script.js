ff4fb769a49482233a8ae7ae0543f93073b58705
nesli9 <doanneslihan@gmail.com>
2022-05-25 16:47:31 +0300

    Değişkenler

---
+++ script.js
@@ -0,0 +1,10 @@
+//alert("merhaba!");
+console.log("merhaba");
+console.log(1234);
+console.log(true);
+console.log([1,2,3]);
+console.error("hata oluştu");
+console.warn("bir uyarı oluştu");
+
+
+console.clear();
---
+++ script2.js
@@ -0,0 +1,11 @@
+var age;
+console.log(age);
+age=20;
+console.log(age);
+
+var fullname='sadık turan';
+console.log(fullname);
+
+fullname='çınar turan';
+console.log(fullname);
+
