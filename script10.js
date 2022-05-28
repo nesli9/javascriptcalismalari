300a16a0d3b3e3b02ccb34d22ca9649786e431db
nesli9 <doanneslihan@gmail.com>
2022-05-28 20:34:51 +0300

    1

---
+++ script10.js
@@ -0,0 +1,17 @@
+// strings
+
+const firstName = 'Sadık';
+const lastName = 'Turan';
+const age = 35;
+let val ; 
+
+//string concat
+
+val = firstName +' '+ lastName;
+val = 'Sadık';
+val += ' Turan';
+
+val = 'Benim adım ' +firstName+' '+lastName+ ' ve yaşım '+' ' +age ;
+
+console.log(val);
+console.log(typeof val);
