a7afac65dbe24e11b12ba57db25bb87fe8cf7afb
nesli9 <doanneslihan@gmail.com>
2022-05-30 22:55:03 +0300

    if / else

---
+++ script15.js
@@ -0,0 +1,54 @@
+// if /else statements
+
+const firstName ='Sena';
+const age = 19;
+const isStudent = false;
+//const school='university';
+const school='high school';
+
+// if(firstName === 'Sena'){
+//     console.log('Merhaba Sena');
+// }
+
+// if(age === 19){
+//     console.log('Yaşınız 19');
+// }
+
+// if(isStudent == true){
+//     console.log('Merhaba Öğrenci');
+// }else{
+//     console.log('Merhaba işler nasıl');
+// }
+
+// if( age >= 18){
+
+
+//     if((school=='university') || (school=='high school')){
+//         console.log('Ehliyet alabilirsiniz');
+//     }else{
+//         console.log('Eğitim durumunuz yetersiz');
+//     }
+    
+
+// }else{
+//     console.log('Ehliyet alamazsınız');
+// }
+
+// if( age >0 && age<12){
+//     console.log(`${firstName} is a child`);
+// }else if (age>=13 && age <=19){
+//     console.log(`${firstName} is a teenager`);
+// }else{
+//     console.log(`${firstName} is a adult`);
+// }
+
+//undefined
+let id=' 12355';
+
+if(typeof id !=='undefined'){
+    console.log('id :'+id);
+
+}else{
+    console.log('no id');
+}
+
