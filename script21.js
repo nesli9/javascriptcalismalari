d29c2fcf9abea2f0bce72241e4ae67acf2954e49
nesli9 <doanneslihan@gmail.com>
2022-05-31 19:55:51 +0300

    Loops in Array & Object

---
+++ script21.js
@@ -0,0 +1,55 @@
+//Loops in Array & Object
+
+let cars =['BMW','Mercedes','Toyota'];
+let people =[
+    {firstName : 'Ada' , lastName : 'Bilgi'},
+    {firstName : 'Yiğit' , lastName : 'Bilgi'},
+    {firstName : 'Çınar' , lastName : 'Turan'}
+
+];
+
+
+// for(let i=0;i<cars.length;i++){
+//     console.log(cars[i]);
+// }
+
+// Arrays
+// for (let i = 0;i<people.length;i++){
+//     console.log(people[i].firstName);
+// }
+
+// Arrays
+//for-in
+// for(let i in cars){
+//     console.log(`index : ${i} value : ${cars[i]}`);
+//}
+
+//Object
+// for (let i in people){
+//     console.log(`index : ${i} value : ${people[i].firstName}`);
+// }
+
+
+//foreach
+// cars.forEach(function(item){
+//     console.log(item);
+// });
+
+// people.forEach(function(item){
+//     console.log(item.firstName);
+// });
+
+//map : returns an array
+
+
+// let val = people.map(function(item){
+//     return IIRFilterNode.firstName + ' ' + item.lastName;
+// });
+
+// console.log(val);
+
+let numbers = [1,5,6,8,10];
+let num = numbers.map(function(n){
+    return n*n;
+});
+console.log(num);
