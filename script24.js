ffd47a0ea327eee5eec90b2d1f6e207ed0ae78ee
nesli9 <doanneslihan@gmail.com>
2022-06-01 23:57:54 +0300

    Function Declarations & Expressions

---
+++ script24.js
@@ -0,0 +1,18 @@
+//Function Declarations 
+
+// function sum(a,b){
+//     var c= a+b;
+//     return c;
+// };
+
+
+// console.log(sum(10,20));
+
+
+//Function Expressions
+
+const sum = function(a,b){
+    var c = a+b;
+    return c;
+};
+console.log(sum(10,30));
