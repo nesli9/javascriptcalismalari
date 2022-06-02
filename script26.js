e6f69d8ecbb867f25c4eb6156bcce69f15bbbb5c
nesli9 <doanneslihan@gmail.com>
2022-06-02 23:03:44 +0300

    window object

---
+++ script26.js
@@ -0,0 +1,49 @@
+//Window Object
+
+let val;
+var a =10;
+function abc(){
+    return 0;
+}
+
+val = window;
+
+//alert
+//alert('merhaba');
+
+//promt
+//var val = prompt('bir sayı giriniz');
+
+
+//confirm
+// val = confirm('emin misiniz?');
+
+// if(val){
+//     console.log('ok');
+// }else{
+//     console.log('no');
+// }
+
+
+//scroll
+// val = window.scrollX;
+// val = scrollY; //window. yazmadanda aynı görevi görür.
+
+
+//location
+val = window.location;
+val = window.location.href;
+val = window.location.hostname;
+val = window.location.host;
+val = window.location.protocol;
+val = window.location.search;
+
+//window.location.href='http://sadikturan.com';
+
+//window.location.reload();
+
+console.log(val);
+
+//navigator
+
+
