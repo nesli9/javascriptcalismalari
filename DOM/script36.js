5c3a2db2f448096080833e1d3414a77a5c32911a
nesli9 <doanneslihan@gmail.com>
2022-06-06 22:11:06 +0300

    Keyboard Events

---
+++ DOM/script36.js
@@ -0,0 +1,30 @@
+// Keyboard Events
+const input = document.querySelector('#txtTaskName');
+const form = document.querySelector('#form');
+
+
+// input.addEventListener('keydown',eventHandler);
+// input.addEventListener('keyup',eventHandler);
+// input.addEventListener('keypress',eventHandler);
+// input.addEventListener('focus',eventHandler);
+// input.addEventListener('blur',eventHandler);
+// input.addEventListener('cut',eventHandler);
+// input.addEventListener('paste',eventHandler);
+//input.addEventListener('select',eventHandler);
+
+
+
+form.addEventListener('submit',eventHandler);
+
+
+function eventHandler(e){
+    console.log('event type : ' +e.type);
+    // console.log('key code :  ' +e.keyCode);
+    // console.log('value :  ' +e.target.value);
+
+    //e.target.style.backgroundColor = 'yellow';
+
+
+}
+
+
