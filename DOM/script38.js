e25fd9232e0c0dee87e164ac65300caf1d52329a
nesli9 <doanneslihan@gmail.com>
2022-06-08 20:29:59 +0300

    .

--- DOM/script38.js
+++ DOM/script38.js
@@ -1,21 +1,23 @@
-// LOCAL STORAGE
+// LOCAL STORAGE  //yapılan uygulamaların bilgilerini tarayıcı hafızasında tutmaya yarar
+                   //belli url ile ilişkilendirilen yapıları tarayıcı kapansa bile saklar
 let val;
 
-// set item
+// set item  //eleman atama
 
 const firstName = localStorage.setItem('firstName','Sadık');
 const lastName = localStorage.setItem('lastName','Turan');
 let hobies = ['sinema','araba','gezmek'];
 
-// get item
+// get item  //eleman alma
 val = localStorage.getItem('firstName');
 val = localStorage.getItem('lastName');
 
-// remove item
+
+// remove item  //item silme
 // localStorage.removeItem('firstName');
 // localStorage.removeItem('lastName');
 
-// clear
+// clear //bütün elemanları silmek için kullanılır
 // localStorage.clear();
 
 // set array to storage
@@ -27,7 +29,7 @@ console.log(val);
 console.log(localStorage);
 
 
-// SESSION STORAGE
+// SESSION STORAGE  //tarayıcı kapandığı anda tuttuğu bilgiler gider
 // const city = sessionStorage.setItem('city','Kocaeli');
 // const country = sessionStorage.setItem('country','Türkiye');
 
