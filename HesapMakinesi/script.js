c7d011c0fdb551b9bbdfd1da2133c3b1edd168bd
nesli9 <doanneslihan@gmail.com>
2022-06-13 19:44:35 +0300

    Hesaplamanın Yapılması

--- HesapMakinesi/script.js
+++ HesapMakinesi/script.js
@@ -1,59 +1,98 @@
 const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
-let displayValue ='0';
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 updateDisplay();
 
-function updateDisplay(){
+function updateDisplay() {
     display.value = displayValue;
 }
 
-keys.addEventListener('click' ,function(e){
+keys.addEventListener('click', function(e) {
     const element = e.target;
 
-    if(!element.matches('button')) return;
+    if (!element.matches('button')) return;
 
-    if(element.classList.contains('operator')){
-        console.log('operator',element.value);
-        return;
-    }
-    if(element.classList.contains('desimal')){
-        //console.log('desimal',element.value);
-        inputDesimal(element.value);
+    if(element.classList.contains('operator')) {
+        // console.log('operator', element.value);
+        handleOperator(element.value);
         updateDisplay();
         return;
     }
-    if(element.classList.contains('clear')){
-        //console.log('clear',element.value);
+
+    if(element.classList.contains('decimal')) {
+        inputDecimal();
+        updateDisplay();
+        return;
+    }
+
+    if(element.classList.contains('clear')) {
         clear();
-        updateDisplay();
+        updateDisplay(); 
         return;
     }
-
-
-    //console.log('number',element.value);
 
     inputNumber(element.value);
     updateDisplay();
 });
 
-function inputNumber(number){
-    displayValue = displayValue ==='0'? number : displayValue+number;
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
 
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
-function inputDesimal(){
-    if(!displayValue.includes('.')){
+function calculate(first, second, operator) {
+    if(operator === '+') {
+        return first + second;
+    } else if (operator === '-') {
+        return first - second;
+    } else if (operator === '*') {
+        return first * second
+    } else if (operator === '/') {
+        return first / second;
+    }
+    return second;
+}
+
+function inputNumber(num) {
+    if(waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === '0'? num: displayValue + num;
+    }
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+}
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
         displayValue += '.';
     }
 }
 
-function clear(){
-    displayValue ='0';
+function clear() {
+    displayValue = '0';
 }
-
-
-
-
-
