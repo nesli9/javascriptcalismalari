f2b38b8d0bc015613d781d70ac2adc806ab2b02c
nesli9 <doanneslihan@gmail.com>
2022-06-13 12:25:58 +0300

    Bilet Bilgilerinin Local Storage ile Saklanması

--- SinemaBiletRezervasyonu/script.js
+++ SinemaBiletRezervasyonu/script.js
@@ -2,6 +2,7 @@ const container =document.querySelector('.container');
 const count = document.getElementById('count');
 const amount =document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
 
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('seat')&& !e.target.classList.contains('reserved')){
@@ -17,11 +18,36 @@ select.addEventListener('change',function(e){
 });
 
 function calculateTotal(){
-    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-    let price = select.value;
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+    const selectedSeatsArray =[];
+    const seatsArray =[];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArray.push(seat);
+    });
+
+    
+    seats.forEach(function(seat){
+        seatsArray.push(seat);
+    });
+    
+    //[1,2,5]
+    let selectedSeatIndexs = selectedSeatsArray.map(function(seat){
+        return seatsArray.indexOf(seat);
+    });
+    
+
+    let selectedSeatCount = selectedSeats.length;
     
     count.innerText = selectedSeatCount;
-    amount.innerText = selectedSeatCount*price;
+    amount.innerText = selectedSeatCount*select.value;
 
 
+    saveToLocalStorage(selectedSeatIndexs);
 }
+function saveToLocalStorage(indexs){
+    localStorage.setItem('selectedSeats',JSON.stringify(indexs));
+    localStorage.setItem('selectedMovieIndex',select.selectedIndex);
+}
+
+
