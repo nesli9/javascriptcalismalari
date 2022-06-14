89f1f0e3ebc7c035f8c7d81e94850b2a83dd41e7
nesli9 <doanneslihan@gmail.com>
2022-06-14 21:40:21 +0300

    object

---
+++ Object&Functions/script.js
@@ -0,0 +1,20 @@
+let val ; 
+
+let num = 10;
+//object literal
+let yigit = {
+    name : 'yiğit',
+    yearOfBirth : 2010,
+    job : 'student'
+}
+//array
+let numbers = [10,20,30];
+
+
+val = num;
+val = yigit;
+val = numbers;
+
+
+console.log(val);
+console.log(typeof val);
