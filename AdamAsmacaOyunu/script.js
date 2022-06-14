e405dcc13eed6fb49055bd9eb589d5df51fa6381
nesli9 <doanneslihan@gmail.com>
2022-06-14 14:57:06 +0300

    Kelimenin Gösterilmesi

---
+++ AdamAsmacaOyunu/script.js
@@ -0,0 +1,35 @@
+const word_element = document.getElementById('word');
+const correctLetters=['j','a','v','s'];
+const wrongLetters=[];
+
+
+function getRandomWord(){
+    const words = ['javascript','java','pyhton'];
+
+    return words[Math.floor(Math.random()*words.length)];
+}
+
+
+
+function displayWord(){
+    const selectedWord = getRandomWord();
+
+    word_element.innerHTML = `
+    ${selectedWord.split('').map(letter => `
+    <div class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+    </div>
+    
+    `).join('')}
+    
+    `;
+    
+    const w =word_element.innerText.replace(/\n/g,'');
+    if(w === selectedWord){
+        console.log('Bildiniz');
+    }
+
+    
+
+}
+displayWord();
