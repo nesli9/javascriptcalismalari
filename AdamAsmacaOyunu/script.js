e42802e587430173290295d944bdaa6db8e1bde4
nesli9 <doanneslihan@gmail.com>
2022-06-14 20:49:38 +0300

    Hatalı Harflerin Gösterilmesi

--- AdamAsmacaOyunu/script.js
+++ AdamAsmacaOyunu/script.js
@@ -1,60 +1,71 @@
-const word_element = document.getElementById('word');
+const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
 const message_el = document.getElementById('success-message');
-const correctLetters=['j','a','v','s'];
-const wrongLetters=[];
+const wrongLetters_el = document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item');
+
+const correctLetters = [];
+const wrongLetters = [];
 const selectedWord = getRandomWord();
 
-
-function getRandomWord(){
-    const words = ['javascript','java','pyhton'];
-
-    return words[Math.floor(Math.random()*words.length)];
+function getRandomWord() {
+    const words = ["javascript","java","python"];
+    return words[Math.floor(Math.random() * words.length)];
 }
 
-
-
-function displayWord(){
-    
-
-    word_element.innerHTML = `
-    ${selectedWord.split('').map(letter => `
-    <div class="letter">
-        ${correctLetters.includes(letter) ? letter : ''}
-    </div>
-    
-    `).join('')}
-    
+function displayWord() {    
+    word_el.innerHTML = `
+        ${selectedWord.split('').map(letter => `
+            <div class="letter">
+                ${correctLetters.includes(letter) ? letter: ''}
+            </div>
+        `).join('')}    
     `;
-    
-    const w =word_element.innerText.replace(/\n/g,'');
-    if(w === selectedWord){
+
+    const w = word_el.innerText.replace(/\n/g,'');
+    if (w === selectedWord) {
         popup.style.display = 'flex';
-        message_el.innerText = 'Tebrikler Kazandınız';
+        message_el.innerText = 'Tebrikler kazandınız.';
     }
+}
 
-    
-    window.addEventListener('keydown' , function(e){
-        if(e.keyCode >= 65 && e.keyCode <= 90){
-            const letter = e.key;
+function updateWrongLetters() {
+    wrongLetters_el.innerHTML = `
+        ${wrongLetters.length>0?'<h3>Hatalı harfler</h3>':''}
+        ${wrongLetters.map(letter=> `<span>${letter}<span>`)}
+    `;
 
-            if(selectedWord.indexOf(letter)){
-                if(!correctLetters.includes(letter)){
-                    correctLetters.push(letter);
-                    displayWord();
-                }else{
-                    //bu harfi zaten ekleniz.
-                }
-            }else{
-                if(!wrongLetters.includes(letter)){
-                    wrongLetters.push(letter);
-                    //hatalı harfleri güncelle
-                }
+    items.forEach((item,index) => {
+        const errorCount = wrongLetters.length;
+
+        if (index<errorCount) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+
+
+}
+
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {        
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+               console.log('bu harfi zaten eklediniz.');
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+                updateWrongLetters();
             }
         }
-        
-    });
+    }
+});
 
-
-}
-displayWord();
+displayWord()
