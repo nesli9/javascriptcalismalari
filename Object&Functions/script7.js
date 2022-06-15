6425c67693e325bc963022585da235bc961bcddb
nesli9 <doanneslihan@gmail.com>
2022-06-15 19:33:43 +0300

    Built-in Constructors

---
+++ Object&Functions/script7.js
@@ -0,0 +1,57 @@
+// String 
+
+var str1 = 'Sadık';
+var str2 = new String('Sadık');
+
+console.log(str1);
+console.log(typeof str1);
+console.log(str2);
+console.log(typeof str2);
+
+if(str2 === 'Sadık'){
+    console.log('yes');
+}  else{
+    console.log('no');
+}
+
+String.prototype.repeat = function(n){
+    return new Array(n+1).join(this);
+}
+
+console.log('sadık'.repeat(4));
+
+
+//Number 
+var num1 = 10;
+var num2 = new Number(10);
+
+//Boolean
+var bool1 = true;
+var bool2 = new Boolean(true);
+
+//Object
+var obj1={
+    name: 'sadık',
+
+}
+var obj2 = new Object({
+    name  : 'sadık'
+});
+
+//Array
+var arr1 = ['ada','sena','çınar','yiğit'];
+var arr2 = new Array ('ada','sena','çınar','yiğit');
+
+Array.prototype.remove = Array.prototype.remove || function(member){  //null mı değil mi kontrolu yapılır.
+    var index = this.indexOf(member);
+
+    if ( index > -1){
+        this.splice(index,1);
+    }
+    return this;
+}
+
+console.log(arr1.remove('sena')) ;
+
+
+
