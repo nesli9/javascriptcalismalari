15dcb6ba5fa0cb9a428b41c18834cd63b8fe8ce8
nesli9 <doanneslihan@gmail.com>
2022-06-15 16:30:33 +0300

    object.create

---
+++ Object&Functions/script5.js
@@ -0,0 +1,29 @@
+// object.create
+
+let personProto = {
+    calculateAge : function(){
+        return 2018 - this.yearOfBirth;
+    }
+}
+
+let yigit = Object.create(personProto);
+
+yigit.name = 'yiğit';
+yigit.yearOfBirth = 2010;
+yigit.job = 'student';
+
+let emel = Object.create(personProto,{
+    name : {value : 'emel'},
+    yearOfBirth : {value : 1989},
+    job : {value : 'teacher'}
+});
+
+
+console.log(emel);
+console.log(emel.calculateAge());
+
+console.log(yigit.calculateAge());
+
+console.log(personProto);
+
+
