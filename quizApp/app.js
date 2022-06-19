b4cbb5c9c309f9213a5fefa410f9dbe56cb5d76c
nesli9 <doanneslihan@gmail.com>
2022-06-19 22:34:23 +0300

    Quiz Objesinin Oluşturulması

--- quizApp/app.js
+++ quizApp/app.js
@@ -9,17 +9,59 @@ Question.prototype.checkAnswer = function(answer){
     return this.answer === answer;
 }
 
+// Quiz Constructor
+function Quiz(questions){
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0
+}
+
+// Quiz Prototype
+Quiz.prototype.getQuestion = function(){
+    return this.questions[this.questionIndex];
+}
+
+// Quiz isFinish
+Quiz.prototype.isFinish = function(){
+    return this.questions.length === this.questionIndex;
+}
+
+// Quiz guess
+Quiz.prototype.guess = function(answer){
+    var question = this.getQuestion();
+
+    if(question.checkAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
+
 var q1 = new Question("what's the best programming language ?",["C#","javascript","pyhton","asp.net"],"javascript");
 
 var q2 = new Question("what's the most popular programming language ?",["c#","visual basic","nodejs","javascript"],"javascript");
 
 var q3 = new Question("what's the best modern programming language ?",["C#","javascript","pyhton","asp.net"],"javascript");
 
+var q4 = new Question("what's the best modern programming language ?",["C#","javascript","pyhton","asp.net"],"javascript");
 
-console.log(q1.checkAnswer('c#'));
-console.log(q1.checkAnswer('javascript'));
-
-console.log(q2.checkAnswer('visual basic'));
-console.log(q2.checkAnswer('javascript'));
+var questions = [q1,q2,q3,q4];
 
 
+// Start Quiz
+
+var quiz = new Quiz(questions);
+
+console.log(quiz.isFinish());
+
+console.log(quiz.getQuestion());
+quiz.guess('javascript');
+
+console.log(quiz.getQuestion());
+quiz.guess('javascript');
+
+console.log(quiz.getQuestion());
+quiz.guess('javascript');
+
+console.log(quiz.score)
+console.log(quiz.isFinish());
