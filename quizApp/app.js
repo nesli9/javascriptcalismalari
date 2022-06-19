241e5339867327c4bf6612a3f3af6f0f8871ad81
nesli9 <doanneslihan@gmail.com>
2022-06-19 22:37:50 +0300

    Arayüzün Oluşturulması

--- quizApp/app.js
+++ quizApp/app.js
@@ -43,25 +43,64 @@ var q2 = new Question("what's the most popular programming language ?",["c#","vi
 
 var q3 = new Question("what's the best modern programming language ?",["C#","javascript","pyhton","asp.net"],"javascript");
 
-var q4 = new Question("what's the best modern programming language ?",["C#","javascript","pyhton","asp.net"],"javascript");
+var q4 = new Question("what's language ?",["C#","javascript","css","asp.net"],"javascript");
 
-var questions = [q1,q2,q3,q4];
+var q5 = new Question("modern programming language ?",["C#","html","pyhton","pyhton"],"javascript");
+
+var questions = [q1,q2,q3,q4,q5];
 
 
 // Start Quiz
 
 var quiz = new Quiz(questions);
 
-console.log(quiz.isFinish());
+loadQuestion();
 
-console.log(quiz.getQuestion());
-quiz.guess('javascript');
 
-console.log(quiz.getQuestion());
-quiz.guess('javascript');
+function loadQuestion(){
+    if(quiz.isFinish()){
+        showScore();
+    }else{
 
-console.log(quiz.getQuestion());
-quiz.guess('javascript');
+        var question = quiz.getQuestion();
+        var choices = question.choices;
+        
+        document.querySelector('#question').textContent = question.text;
 
-console.log(quiz.score)
-console.log(quiz.isFinish());
+        for(var i=0; i<choices.length;i++){
+            var element = document.querySelector('#choice'+i);
+            element.innerHTML = choices[i];
+            guess('btn'+i,choices[i]);
+        }
+
+        showProgress();
+    }
+}
+
+function guess(id,guess){
+    var btn = document.getElementById(id);
+    btn.onclick = function(){
+        quiz.guess(guess);
+        loadQuestion()
+    }
+}
+
+function showScore(){
+   var html = `<h2>Score</h2><h4>${quiz.score}</h4>`;
+
+   document.querySelector('.card-body').innerHTML = html;
+}
+
+function showProgress(){
+    var totalQuestion = quiz.questions.length;
+    var questionNumber = quiz.questionIndex+1;
+    var html = 'Question '+ questionNumber + ' of ' + totalQuestion;
+
+    if(totalQuestion === questionNumber){
+        document.querySelector('#progress').innerHTML = "Quiz is Ended";
+    }else{
+        document.querySelector('#progress').innerHTML = html;
+    }
+
+  
+}
