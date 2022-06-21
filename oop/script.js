31b49146b44ac85e08cb2a954eedb9ca344e3454
nesli9 <doanneslihan@gmail.com>
2022-06-21 21:05:35 +0300

    Prototype

--- oop/script.js
+++ oop/script.js
@@ -45,9 +45,12 @@ function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
     this.soruMetni = soruMetni;
     this.cevapSecenekleri = cevapSecenekleri;
     this.dogruCevap = dogruCevap;
-    this.cevabiKontrolEt = function(cevap) {
-        return cevap === this.dogruCevap
-    }
+
+    console.log(this);
+}
+
+Soru.prototype.cevabiKontrolEt = function(cevap) {
+    return cevap === this.dogruCevap
 }
 
 let soru1 = new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c");
@@ -60,9 +63,7 @@ let sorular = [
     new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
 ]
 
-console.log(soru1.soruMetni);
-console.log(soru1.dogruCevap);
+
 console.log(soru1.cevabiKontrolEt("c"));
-console.log(soru2.cevabiKontrolEt("a"));
 
 
