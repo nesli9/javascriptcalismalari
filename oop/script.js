56a6613eeae19f1d3a8a0fafbc5651b283b437dd
nesli9 <doanneslihan@gmail.com>
2022-06-21 20:59:58 +0300

    Nesne Oluşturma : Constructor

--- oop/script.js
+++ oop/script.js
@@ -1,35 +1,68 @@
+// // OOP: Nesne Tabanlı Programlama
+// // Object
+
+// let soru = {
+//     soruMetni: "Hangisi javascript paket yönetim uygulasıdır?",
+//     cevapSecenekleri: {
+//         a: "Node.js",
+//         b: "Typescript",
+//         c: "Npm"
+//     },
+//     dogruCevap: "c",
+//     cevabiKontrolEt: function(cevap) {
+//         return cevap === this.dogruCevap
+//     }
+// }
+
+// let soru2 = {
+//     soruMetni: "Hangisi .net paket yönetim uygulasıdır?",
+//     cevapSecenekleri: {
+//         a: "Node.js",
+//         b: "nuget",
+//         c: "Npm"
+//     },
+//     dogruCevap: "b",
+//     cevabiKontrolEt: function(cevap) {
+//         return cevap === this.dogruCevap
+//     }
+// }
+
+// console.log(soru.soruMetni);
+// console.log(soru.cevabiKontrolEt("c"));
+// console.log(soru2.cevabiKontrolEt("b"));
+
+// // Sınıf, Constructor => nesne * 30
+// // ES5, ES6, ES7
+
+
 // OOP: Nesne Tabanlı Programlama
 // Object
 
-let soru = {
-    soruMetni: "Hangisi javascript paket yönetim uygulasıdır?",
-    cevapSecenekleri: {
-        a: "Node.js",
-        b: "Typescript",
-        c: "Npm"
-    },
-    dogruCevap: "c",
-    cevabiKontrolEt: function(cevap) {
-        return cevap === this.dogruCevap
-    }
-}
-
-let soru2 = {
-    soruMetni: "Hangisi .net paket yönetim uygulasıdır?",
-    cevapSecenekleri: {
-        a: "Node.js",
-        b: "nuget",
-        c: "Npm"
-    },
-    dogruCevap: "b",
-    cevabiKontrolEt: function(cevap) {
-        return cevap === this.dogruCevap
-    }
-}
-
-console.log(soru.soruMetni);
-console.log(soru.cevabiKontrolEt("c"));
-console.log(soru2.cevabiKontrolEt("b"));
-
 // Sınıf, Constructor => nesne * 30
 // ES5, ES6, ES7
+
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+    this.soruMetni = soruMetni;
+    this.cevapSecenekleri = cevapSecenekleri;
+    this.dogruCevap = dogruCevap;
+    this.cevabiKontrolEt = function(cevap) {
+        return cevap === this.dogruCevap
+    }
+}
+
+let soru1 = new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c");
+let soru2 = new Soru("Hangisi .net paket yönetim uygulasıdır?", { a: "Node.js", b: "Nuget", c: "Npm" }, "b");
+
+let sorular = [
+    new Soru("Hangisi js paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
+]
+
+console.log(soru1.soruMetni);
+console.log(soru1.dogruCevap);
+console.log(soru1.cevabiKontrolEt("c"));
+console.log(soru2.cevabiKontrolEt("a"));
+
+
