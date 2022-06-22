084bc2369622600812dd7642b1bd00b4ff3e2737
nesli9 <doanneslihan@gmail.com>
2022-06-23 00:09:35 +0300

    Soruların Gösterilmesi

--- oop/script.js
+++ oop/script.js
@@ -59,10 +59,10 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 // let soru2 = new Soru("Hangisi .net paket yönetim uygulasıdır?", { a: "Node.js", b: "Nuget", c: "Npm" }, "b");
 
 let sorular = [
-    new Soru("1-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("2-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("3-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("4-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
+    new Soru("1-Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" , d: "Nuget"}, "c"),
+    new Soru("2-Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("3-Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("4-Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
 ]
 
 function Quiz(sorular){
@@ -78,17 +78,41 @@ const quiz = new Quiz(sorular);
 
 
 document.querySelector(".btn_start").addEventListener("click", function(){
-    if(quiz.sorular.length != quiz.soruIndex){
+    
         document.querySelector(".quiz_box").classList.add("active");
-        console.log(quiz.soruGetir());
-        quiz.soruIndex += 1;
-    }else {
-        console.log("Quiz Bitti!");
-    }
+        soruGoster(quiz.soruGetir());
+        
     
 });
 
-
 //console.log(soru1.cevabiKontrolEt("c"));
+
+document.querySelector(".next_btn").addEventListener("click",function(){
+    if(quiz.sorular.length != quiz.soruIndex +1){
+        quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
+    }else {
+        console.log("Quiz Bitti!");
+    }
+});
+
+function soruGoster(soru){
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = '';
+
+    for(let cevap in soru.cevapSecenekleri){
+        options +=
+            `
+                <div class="option">
+                    <span> <b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]} </span>
+                </div>
+            `;
+    }
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+
+}
+
+
 
 
