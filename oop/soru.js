0d35173ed0b17426e13eeca65a06b836e449e033
nesli9 <doanneslihan@gmail.com>
2022-06-25 16:33:55 +0300

    Cevaplama Süresinin Eklenmesi

--- oop/soru.js
+++ oop/soru.js
@@ -2,8 +2,6 @@ function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
     this.soruMetni = soruMetni;
     this.cevapSecenekleri = cevapSecenekleri;
     this.dogruCevap = dogruCevap;
-
-    //console.log(this);
 }
 
 Soru.prototype.cevabiKontrolEt = function(cevap) {
@@ -12,10 +10,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 
 let sorular = [
     new Soru("1-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" , d: "Nuget" }, "c"),
-    new Soru("2-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("3-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("4-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
+    new Soru("2-Hangisi frontend kapsamında değerlendirilmez?", { a: "css", b: "html", c: "javascipt", d: "sql" }, "d"),
+    new Soru("3-Hangisi backend kapsamında değerlendirilir?", { a: "node.js", b: "typescript", c: "angular", d: "react" }, "a"),
+    new Soru("4-Hangisi javascript programlama dilini kullanmaz?", { a: "react", b: "angular", c: "vuejs", d: "asp.net" }, "d")
 ];
-
-
-
