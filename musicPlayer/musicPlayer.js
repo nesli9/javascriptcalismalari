2fe482e9f5eb46868960dfc3ba68c0e1df23f9e6
nesli9 <doanneslihan@gmail.com>
2022-07-01 23:36:27 +0300

    Music Player - Controls

--- musicPlayer/musicPlayer.js
+++ musicPlayer/musicPlayer.js
@@ -1,7 +1,7 @@
 class MusicPlayer {
     constructor(musicList) {
         this.musicList = musicList;
-        this.index = 0;
+        this.index = 1;
     }
 
     getMusic() {
@@ -17,7 +17,7 @@ class MusicPlayer {
         }
     }
 
-    previous() {
+    prev() {
         if(this.index != 0) {
             this.index--;
         } else {
