c2563e547a9223ea8c035be84704f1afa1d10758
nesli9 <doanneslihan@gmail.com>
2022-07-25 00:15:58 +0300

    Data Yapısının Oluşturulması

--- oemCalculator/app.js
+++ oemCalculator/app.js
@@ -8,7 +8,33 @@ const StorageController = (function(){
 //Product Controller
 const ProductController = (function(){
 
+    //private
+    const Product = function(id , name,price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
+    const data ={
+        products: [
+            {id: 0 , name: 'Monitor' , price: 100},
+            {id: 1 , name: 'Ram' , price: 30},
+            {id: 2 , name: 'Klavye' , price: 10},
+        ],
+        selectedProduct:null,
+        totalPrice:0
+    }
+    
+    //public
+    return{
+
+        getProducts: function(){
+            return data.products;
+        },
+        getData: function(){
+            return data;
+        }
+    }
 
 
 })();
@@ -22,9 +48,23 @@ const UIController = (function(){
 
 
 //App Controller
-const AppController = (function(ProductCtrl,UICtrl){
+const App = (function(ProductCtrl,UICtrl){
 
+    //private
+    
 
-})();
+    //public
+    return{
+        init : function(){
+            console.log('starting app....');
+            const products = ProductCtrl.getProducts();
+            //console.log(products);
 
+            UICtrl.createProductList(products);
 
+        }
+    }
+
+})(ProductController,UIController);
+
+App.init();
