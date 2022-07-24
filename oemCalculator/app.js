0e074c4f2facfd44e81398bf358837ece94d252a
nesli9 <doanneslihan@gmail.com>
2022-07-25 00:05:55 +0300

    Modüllerin Oluşturulması

--- oemCalculator/app.js
+++ oemCalculator/app.js
@@ -1,9 +1,30 @@
+// Storage Controller
+const StorageController = (function(){
+
+
+})();
+
+
+//Product Controller
+const ProductController = (function(){
 
 
 
 
+})();
 
 
+//UI Controller
+const UIController = (function(){
 
+
+})();
+
+
+//App Controller
+const AppController = (function(ProductCtrl,UICtrl){
+
+
+})();
 
 
