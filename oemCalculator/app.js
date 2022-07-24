9b53539515831d25e0a8fa03bf3268dd43378e54
nesli9 <doanneslihan@gmail.com>
2022-07-25 00:31:41 +0300

    Liste Elemanlarının Oluşturulması

--- oemCalculator/app.js
+++ oemCalculator/app.js
@@ -20,6 +20,7 @@ const ProductController = (function(){
             {id: 0 , name: 'Monitor' , price: 100},
             {id: 1 , name: 'Ram' , price: 30},
             {id: 2 , name: 'Klavye' , price: 10},
+            {id: 3 , name: 'Mouse' , price: 5},
         ],
         selectedProduct:null,
         totalPrice:0
@@ -42,6 +43,39 @@ const ProductController = (function(){
 
 //UI Controller
 const UIController = (function(){
+
+    const Selectors = {
+        productList : "#item-list" //id bilgisi direk buraya gelir (html sayfasındaki item-list kısmı değiştirilirse bu controllerda sadece bu kısmın ismini değiştrilmesi yeterli olur)
+    }
+
+    return{
+        createProductList: function(products){
+            let html=``;
+
+
+            products.forEach(prd => {
+                html += `
+                <tr>
+                <td>${prd.id}</td>
+                <td>${prd.name}</td>
+                <td>${prd.price}</td>
+                <td class="text-right">
+                    <button type="submit" class="btn btn-warning btn-sm">
+                        <i class="fas fa-edit"></i>
+                        
+                    </button>
+                </td>
+                
+                </tr>
+                `;
+            });
+            
+            document.querySelector(Selectors.productList).innerHTML = html;
+        },
+        getSelectors : function(){
+            return Selectors;
+        }
+    }
 
 
 })();
