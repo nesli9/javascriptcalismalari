af57e066b5ac53471725550ca742082ce3fb1364
nesli9 <doanneslihan@gmail.com>
2022-07-31 14:37:08 +0300

    npm paket kurulumu

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,6 @@
-console.log('hello world');
+var _ = require('underscore');
 
-var a=10;
-var b = 20;
-console.log(a+b);
-
-console.log(window);
+var numbers = [10, 5, 100, 2, 1000];
 
 
-
-
-
-
-
+console.log(_.min(numbers));
