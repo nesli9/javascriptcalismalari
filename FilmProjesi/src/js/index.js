be42fe39dda7f88e81ed18be030fd800df35ff37
nesli9 <doanneslihan@gmail.com>
2022-08-23 21:08:54 +0300

    filmProjesi

--- FilmProjesi/src/js/index.js
+++ FilmProjesi/src/js/index.js
@@ -5,10 +5,58 @@
 
 // model-view-controller
 
+
 import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from './base';
+import * as searchView from './views/searchView';
+import * as movieView from './views/movieView';
+import { Movie } from './models/Movie';
 
-const search = new Search('abc');
+const state = {};
 
-console.log(search);
+// Search Controller
 
-search.getResults();
+const searchController = async () => {
+    const keyword = elements.searchInput.value;
+
+    if (keyword) {
+        state.search = new Search(keyword);
+
+        searchView.clearInput();
+        searchView.clearResults();
+
+        renderLoader(elements.movieListContainer);
+
+        await state.search.getResults();
+        searchView.displayResults(keyword, state.search.data);
+        setTimeout(() => { clearLoader(elements.movieListContainer); }, 1000);
+
+    } else {
+        alert('anahtar kelime girmelisiniz');
+    }
+}
+
+elements.searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchController();
+    console.log("form submitted");
+});
+
+// Movie Controller
+
+const movieController = async () => {
+    const id = window.location.hash.replace('#', '');
+    if (id) {
+        state.movie = new Movie(id);
+
+        renderLoader(elements.movieDetailsContainer);
+
+        await state.movie.getMovie();
+        movieView.backToTop();
+        movieView.displayMovie(state.movie.data);
+        setTimeout(() => { clearLoader(elements.movieDetailsContainer); }, 1000);
+    }
+};
+
+window.addEventListener('hashchange', movieController);
+elements.movieDetailsClose.addEventListener('click', movieView.closeDetails);
