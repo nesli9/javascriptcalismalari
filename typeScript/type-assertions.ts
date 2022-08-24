96fd38160709c9790d11b1ed3d6c40a2b933bd5d
nesli9 <doanneslihan@gmail.com>
2022-08-24 13:29:57 +0300

    Type Assertions

---
+++ typeScript/type-assertions.ts
@@ -0,0 +1,8 @@
+let message ;
+
+message = ' Hello world';
+
+let count = (<string>message).length; //any olarak tanımlanan değisken parantez ve küçük büyük işaretleriyle belirlenen türde olduğu belirtilebilir.
+let length = (message as string).length; //message değişkeni string türüne dönüştürldü
+
+
