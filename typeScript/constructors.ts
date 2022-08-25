b79074e5e78fb735f9bb9f1ba74f45a1808f36ec
nesli9 <doanneslihan@gmail.com>
2022-08-25 13:35:11 +0300

    Constructors

--- typeScript/constructors.ts
+++ typeScript/constructors.ts
@@ -10,7 +10,14 @@ interface Vehicle {
 }
 
 class Taxi implements Vehicle{
-    //..
+    color : string;
+
+    constructor(location?: Point , color?: string){ //contructor ile nesnenin 3 farklı versdiyonu hazırlanmış oldu . hiç bilgi olmayan ,bir bilgi olan veya iki bilgide olan
+        this.currentLocation  = location;
+        this.color = color;
+    }
+
+
     currentLocation : Point;
     travelTo(point : Point):void {
         console.log(`Taksi X:${point.x} Y:${point.y} konumuna gidiyor`);
@@ -25,19 +32,12 @@ class Bus implements Vehicle{
     };
 }
 
-let taxi_1 : Taxi = new Taxi();
+let taxi_1 : Taxi = new Taxi({x:1 ,y:5});
 taxi_1.travelTo({x:1,y:2});
-taxi_1.currentLocation = {x:1 ,y:5};
+//taxi_1.color = 'red';
 
-let taxi_2  = new Taxi();
-taxi_2.travelTo({x:5,y:6});
-taxi_2.currentLocation = {x:3 ,y:4};
+console.log(taxi_1.currentLocation);
 
-let bus_1 = new Bus();
-// bus_1.travelTo({x:1,y:8});
-// bus_1.currentLocation = {x:3 ,y:4};
-
-
-console.log(taxi_1.currentLocation.x);
-console.log(taxi_1.currentLocation.y);
+let taxi_2 = new Taxi({x:2 , y:5},'red');
+let taxi_3 = new Taxi();
 
